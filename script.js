eb11a42f6e9b3ee9c168d081122a906749b7b96a
xrob101 <robertssekyene05@gmail.com>
2025-03-15 17:36:33 +0300

    feat: add choice funtions     - getComputerChoice     - getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+/*
+a function that returns a random choice that is either
+rock, paper or scissors
+*/
+function getComputerChoice() {
+    // returns a number between 0(inclusive), 1 and 2 inclusive
+    const choice =  Math.floor((Math.random() * 3));
+    if (choice === 0) return "rock";
+    else if (choice == 1) return "paper";
+    else return "scisscors";
+}
+
+// console.log(getComputerChoice());
+
+function getHumanChoice() {
+    const input = prompt('Enter "rock", "paper" or "scissors":');
+    return input.toLowerCase();
+    
+}
+
+// console.log(getHumanChoice());
