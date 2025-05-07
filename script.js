58efe5baafecda1f693d8ada074def3579af8c5d
xrob101 <robertssekyene05@gmail.com>
2025-05-07 16:09:20 +0300

    feat: Add new feature to enhance user experience
    this enables users to interact with the game in
    a more friendly way.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ returns a random choice that is either
 rock, paper or scissors
 */
 function getComputerChoice() {
-    // returns a number between 0(inclusive), 1 and 2 inclusive
-    const choice =  Math.floor((Math.random() * 3));
-    if (choice === 0) return "rock";
-    else if (choice == 1) return "paper";
-    else return "scissors";
+  // returns a number between 0(inclusive), 1 and 2 inclusive
+  const choice =  Math.floor((Math.random() * 3));
+  if (choice === 0) return "rock";
+  else if (choice == 1) return "paper";
+  else return "scissors";
 }
 
 
@@ -15,7 +15,7 @@ function getComputerChoice() {
 returns a choice made by user
 */
 function getHumanChoice() { 
-    return(prompt('Enter "rock", "paper" or "scissors":'));
+  return(prompt('Enter "rock", "paper" or "scissors":'));
 }
 
 
@@ -25,88 +25,95 @@ compares the humanChoice and computerChoice
 and prints the result to the console
 */
 function playRound(humanChoice, computerChoice) {
-    printChoices(humanChoice, computerChoice);
-    const humanSelection = humanChoice.toLowerCase();
-    if (humanSelection === "rock") {
-        if (computerChoice === "scissors") {
-            console.log("%cYou Win! Rock beats Scissors", "font-size: 16px; font-weight: 900; color: green");
-            humanScore++;
-        }
-        else if (computerChoice === "paper") {
-            console.log("%cYou Loose! Paper beats Rock", "font-size: 16px; font-weight: 900; color: red");
-            computerScore++;
-        }
-        else console.log("%cIt's a tie", "font-size: 16px; font-weight: 900; color: orange");
-        return 0;
-    } else if (humanSelection === "paper") {
-        if (computerChoice === "scissors") {
-            console.log("%cYou Lose! Scissors beat Paper", "font-size: 16px; font-weight: 900; color: red");
-            computerScore++;
-        }
-        else if (computerChoice === "rock") {
-            console.log("%cYou Win! Paper beats Rock", "font-size: 16px; font-weight: 900; color: green");
-            humanScore++;
-        }
-        else console.log("%cIt's a tie", "font-size: 16px; font-weight: 900; color: orange");
-        return 0;
-    } else if (humanSelection === "scissors") {
-        if (computerChoice === "paper") {
-            console.log("%cYou Win! Scissors beat Paper", "font-size: 16px; font-weight: 900; color: green");
-            humanScore++;
-        }
-        else if (computerChoice === "rock") {
-            console.log("%cYou Loose! Rock beats Scissors", "font-size: 16px; font-weight: 900; color: red");
-            computerScore++;
-        }
-        else console.log("%cIt's a tie", "font-size: 16px; font-weight: 900; color: orange");
-        return 0;
-    } else {
-        console.log("%cYou entered wrong spelling! Replay the round", "font-size: 16px; font-weight: 900; color: yellow")
-        return 1;
+  printChoices(humanChoice, computerChoice);
+  const humanSelection = humanChoice.toLowerCase();
+  const result = document.createElement('h2');
+if (humanSelection === "rock") {
+    if (computerChoice === "scissors") {
+      result.className = 'win';
+      result.textContent = "You Win! Rock beats Scissors";
     }
+    else if (computerChoice === "paper") {
+      result.className = 'lose';
+      result.textContent = "You Lose! Paper beats Rock";
+    }
+    else {
+      result.className = 'tie';
+      result.textContent = "It's a tie";
+    }
+    output.appendChild(result);
+    return 0;
+} else if (humanSelection === "paper") {
+    if (computerChoice === "scissors") {
+      result.className = 'lose';
+      result.textContent = 'You Lose! Scissors beat Paper';
+    }
+    else if (computerChoice === "rock") {
+      result.className = 'win';
+      result.textContent = 'You Win! Paper beats Rock';
+    }
+    else {
+      result.className = 'tie';
+      result.textContent = "It's a tie";
+    }
+    output.appendChild(result);
+    return 0;
+} else if (humanSelection === "scissors") {
+    if (computerChoice === "paper") {
+      result.className = 'win';
+      result.textContent = 'You Win! Scissors beat Paper';
+    }
+    else if (computerChoice === "rock") {
+      result.className = 'lose';
+      result.textContent = 'You Loose! Rock beats Scissors';
+    }
+    else {
+      result.className = 'tie';
+      result.textContent = "It's a tie";
+    }
+    output.appendChild(result);
+    return 0;
+}
 
 }
 
 function printChoices(humanSelection, computerSelection) {
-    console.log(`You: ${humanSelection}\nComputer: ${computerSelection}\n\n`);
+  output.innerHTML = ""; // clear previous results
+  const humanChoice = document.createElement('h3');
+  const computerChoice = document.createElement('h3');
+
+  humanChoice.textContent = `You: ${humanSelection}`;
+  output.appendChild(humanChoice);
+  computerChoice.textContent = `Computer: ${computerSelection}`;
+  output.appendChild(computerChoice);
+
 }
 
 /*
 executes 5 rounds of playRound and prints the overall results
 */
 function playGame() {    
-    let flag;
-    for(let i = 0; i < 5; i++) {
-        // check if one of the players has 3 scores and remaining rounds are 2 or less
-        if ((humanScore === 3 || computerScore === 3) &&
-            (5 - i) <= 2) break;
-        // console.log(i);
-        // check if one of the players has 2 scores but not both
-        // and one of the players has no score sofar yet remaining with 1 round
-        if (!(humanScore === 2 && computerScore === 2) &&
-            (humanScore === 2 || computerScore === 2) &&
-            (humanScore === 0 || computerScore === 0) &&
-            (5 - i) === 1) break;
-       flag = playRound(getHumanChoice(), getComputerChoice());
-       // repeat the round for wrong spelling
-       if (flag) i--;
-    }
-    if (humanScore > computerScore) console.log("%c\n\nCongratulations!!! You Won!", "font-size: 24px; font-weight: 900; color: green");
-    else if (humanScore < computerScore) console.log("%c\n\nYou Lost", "font-size: 24px; font-weight: 900; color: red");
-    else console.log("%c\n\nIt's a tie", "font-size: 24px; font-weight: 900; color: orange");
-
-    console.log(`\nYour Scores: ${humanScore}\nComputer's Scores: ${computerScore}`);
+  playRound(getHumanChoice(), getComputerChoice());
 }
 
-const playBtn = document.querySelector("#play");
-const resetBtn = document.querySelector("#reset");
+const choices = document.querySelector('.choices');
+const output = document.querySelector('#output');
 
-// global scope for game play
-let humanScore = 0; 
-let computerScore = 0;
-playBtn.addEventListener("click", playGame);
-resetBtn.addEventListener("click", function () {
-    humanScore = 0;
-    computerScore = 0;
-    console.clear();
+choices.addEventListener('click', e => {
+  // finding the choice made by user through event bubbling
+  const choice = e.target.id;
+    switch (choice) {
+      case 'rock':
+        playRound("rock", getComputerChoice());
+        break;
+      case 'paper':
+        playRound("paper", getComputerChoice());
+        break;
+      case 'scissors':
+        playRound("scissors", getComputerChoice());
+        break;
+    }
 });
+
+
+
